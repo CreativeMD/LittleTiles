a000780292f91354332038e121cdb49e252b4121
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-09-24 11:46:19 +0200

    Move getHeighestPoint to LittleStructure

--- src/main/java/com/creativemd/littletiles/common/structure/LittleStructure.java
+++ src/main/java/com/creativemd/littletiles/common/structure/LittleStructure.java
@@ -6,14 +6,19 @@ import java.util.HashMap;
 import javax.annotation.Nullable;
 
 import com.creativemd.creativecore.common.utils.HashMapList;
+import com.creativemd.creativecore.common.utils.WorldUtils;
 import com.creativemd.creativecore.gui.container.SubGui;
 import com.creativemd.littletiles.LittleTiles;
 import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
 import com.creativemd.littletiles.common.utils.LittleTile;
 import com.creativemd.littletiles.common.utils.LittleTile.LittleTilePosition;
+import com.creativemd.littletiles.common.utils.small.LittleTileBox;
 import com.creativemd.littletiles.common.utils.small.LittleTileCoord;
+import com.creativemd.littletiles.common.utils.small.LittleTileSize;
+import com.creativemd.littletiles.common.utils.small.LittleTileVec;
 import com.creativemd.littletiles.utils.PreviewTile;
 
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -88,6 +93,7 @@ public abstract class LittleStructure {
 		registerLittleStructure("door", new LittleDoor());
 		registerLittleStructure("ladder", new LittleLadder());
 		registerLittleStructure("bed", new LittleBed());
+		registerLittleStructure("storage", new LittleStorage());
 	}
 	
 	public static LittleStructure createAndLoadStructure(NBTTagCompound nbt, LittleTile mainTile)
@@ -144,6 +150,7 @@ public abstract class LittleStructure {
 	{
 		if(mainTile != null)
 		{
+			//System.out.println("loading Structure");
 			if(tiles == null)
 			{
 				tiles = new ArrayList<LittleTile>();
@@ -262,7 +269,7 @@ public abstract class LittleStructure {
 	{
 		BlockPos coord = pos.getAbsolutePosition(mainTile.te);
 		Chunk chunk = world.getChunkFromBlockCoords(coord);
-		if(!(chunk instanceof EmptyChunk))
+		if(WorldUtils.checkIfChunkExists(chunk))
 		{
 			//chunk.isChunkLoaded
 			TileEntity tileEntity = world.getTileEntity(coord);
@@ -355,6 +362,126 @@ public abstract class LittleStructure {
 	@SideOnly(Side.CLIENT)
 	public abstract LittleStructure parseStructure(SubGui gui);
 	
+	
+	//====================Helpers====================
+	public LittleTileSize getSize()
+	{
+		int minX = Integer.MAX_VALUE;
+		int minY = Integer.MAX_VALUE;
+		int minZ = Integer.MAX_VALUE;
+		
+		int maxX = Integer.MIN_VALUE;
+		int maxY = Integer.MIN_VALUE;
+		int maxZ = Integer.MIN_VALUE;
+		
+		HashMapList<BlockPos, LittleTile> coords = getTilesSortedPerBlock();
+		if(coords.sizeOfValues() == 0)
+			return null;
+		for (BlockPos coord : coords.getKeys()) {
+			ArrayList<LittleTile> values = coords.getValues(coord);
+			for (int j = 0; j < values.size(); j++) {
+				for (int h = 0; h < values.get(j).boundingBoxes.size(); h++) {
+					LittleTileBox box = values.get(j).boundingBoxes.get(h);
+					minX = Math.min(minX, coord.getX()*16+box.minX);
+					minY = Math.min(minY, coord.getY()*16+box.minY);
+					minZ = Math.min(minZ, coord.getZ()*16+box.minZ);
+					
+					maxX = Math.max(maxX, coord.getX()*16+box.maxX);
+					maxY = Math.max(maxY, coord.getY()*16+box.maxY);
+					maxZ = Math.max(maxZ, coord.getZ()*16+box.maxZ);
+				}
+			}
+			/*
+			minX = Math.min(minX, coord.posX);
+			minY = Math.min(minY, coord.posY);
+			minZ = Math.min(minZ, coord.posZ);
+			
+			maxX = Math.max(maxX, coord.posX);
+			maxY = Math.max(maxY, coord.posY);
+			maxZ = Math.max(maxZ, coord.posZ);*/
+		}
+		
+		return new LittleTileBox(minX, minY, minZ, maxX, maxY, maxZ).getSize();
+	}
+	
+	public LittleTileVec getHighestCenterPoint()
+	{
+		int minYPos = Integer.MAX_VALUE;
+		
+		int minX = Integer.MAX_VALUE;
+		int minY = Integer.MAX_VALUE;
+		int minZ = Integer.MAX_VALUE;
+		
+		int maxYPos = Integer.MIN_VALUE;
+		
+		int maxX = Integer.MIN_VALUE;
+		int maxY = Integer.MIN_VALUE;
+		int maxZ = Integer.MIN_VALUE;
+		
+		HashMapList<BlockPos, LittleTile> coords = getTilesSortedPerBlock();
+		if(coords.sizeOfValues() == 0)
+			return null;
+		for (BlockPos coord : coords.getKeys()) {
+			ArrayList<LittleTile> values = coords.getValues(coord);
+			for (int j = 0; j < values.size(); j++) {
+				for (int h = 0; h < values.get(j).boundingBoxes.size(); h++) {
+					LittleTileBox box = values.get(j).boundingBoxes.get(h);
+					minX = Math.min(minX, coord.getX()*16+box.minX);
+					minY = Math.min(minY, coord.getY()*16+box.minY);
+					minZ = Math.min(minZ, coord.getZ()*16+box.minZ);
+					
+					maxX = Math.max(maxX, coord.getX()*16+box.maxX);
+					maxY = Math.max(maxY, coord.getY()*16+box.maxY);
+					maxZ = Math.max(maxZ, coord.getZ()*16+box.maxZ);
+				}
+				minYPos = Math.min(minYPos, coord.getY());
+				maxYPos = Math.max(maxYPos, coord.getY());
+			}
+			/*
+			minX = Math.min(minX, coord.posX);
+			minY = Math.min(minY, coord.posY);
+			minZ = Math.min(minZ, coord.posZ);
+			
+			maxX = Math.max(maxX, coord.posX);
+			maxY = Math.max(maxY, coord.posY);
+			maxZ = Math.max(maxZ, coord.posZ);*/
+		}
+		
+		//double test = Math.floor(((minX+maxX)/16D/2D));
+		int centerX = (int) Math.floor((minX+maxX)/16D/2D);
+		int centerY = (int) Math.floor((minY+maxY)/16D/2D);
+		int centerZ = (int) Math.floor((minZ+maxZ)/16D/2D);
+		
+		int centerTileX = (int) (Math.floor(minX+maxX)/2D)-centerX*16;
+		int centerTileY = (int) (Math.floor(minY+maxY)/2D)-centerY*16;
+		int centerTileZ = (int) (Math.floor(minZ+maxZ)/2D)-centerZ*16;
+		
+		LittleTileVec position = new LittleTileVec((minX+maxX)/2, minYPos*16, (minZ+maxZ)/2);
+		//position.y = ;
+		for (int y = minYPos; y <= maxYPos; y++) {
+			ArrayList<LittleTile> tilesInCenter = coords.getValues(new BlockPos(centerX, y, centerZ));
+			if(tilesInCenter != null)
+			{
+				LittleTileBox box = new LittleTileBox(centerTileX, LittleTile.minPos, centerTileZ, centerTileX+1, LittleTile.maxPos, centerTileZ+1);
+				//int highest = LittleTile.minPos;
+				for (int i = 0; i < tilesInCenter.size(); i++) {
+					for (int j = 0; j < tilesInCenter.get(i).boundingBoxes.size(); j++) {
+						LittleTileBox littleBox = tilesInCenter.get(i).boundingBoxes.get(j);
+						if(box.intersectsWith(littleBox))
+						{
+							position.y = Math.max(y*16+littleBox.maxY, position.y);
+							//highest = Math.max(highest, littleBox.maxY);
+						}
+					}
+				}
+				
+			}
+		}
+		
+		
+		return position;
+	}
+	
 	//====================LittleStructure ID====================
 	
 	public String getIDOfStructure()
@@ -390,6 +517,11 @@ public abstract class LittleStructure {
 	{
 		return false;
 	}
+	
+	public EnumFacing getBedDirection(IBlockState state, IBlockAccess world, BlockPos pos)
+    {
+        return EnumFacing.EAST;
+    }
 
 	public boolean isLadder()
 	{
