ad76eb61642375fbe201bf66bba52af13c45367a
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-01-19 17:41:36 +0100

    Fixed not rendering animated doors with shaders properly

---
+++ src/main/java/shadersmod/client/ShadersRender.java
@@ -0,0 +1,22 @@
+package shadersmod.client;
+
+import net.minecraft.util.BlockRenderLayer;
+
+public class ShadersRender {
+	
+	public static void preRenderChunkLayer(BlockRenderLayer blockLayerIn)
+    {
+		
+    }
+
+    public static void postRenderChunkLayer(BlockRenderLayer blockLayerIn)
+    {
+    	
+    }
+
+	public static void setupArrayPointersVbo()
+	{
+		
+	}
+
+}
