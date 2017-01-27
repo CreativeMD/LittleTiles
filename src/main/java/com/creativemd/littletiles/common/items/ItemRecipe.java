82d4b88d7ced12c5e14c66e76519290ca83c1231
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-01-27 10:22:55 +0100

    Fixed recipe background is not rendering in certain guis

--- src/main/java/com/creativemd/littletiles/common/items/ItemRecipe.java
+++ src/main/java/com/creativemd/littletiles/common/items/ItemRecipe.java
@@ -314,8 +314,8 @@ public class ItemRecipe extends Item implements IExtendedCreativeRendered, IGuiC
 		Minecraft mc = Minecraft.getMinecraft();
 		GlStateManager.pushMatrix();
 		
-		if(cameraTransformType == TransformType.GUI)
-			GlStateManager.translate(0, 0, -10);
+		//if(cameraTransformType == TransformType.GUI)
+			//GlStateManager.translate(0, 0, -10);
 		
 		if(cameraTransformType == TransformType.GUI || !stack.hasTagCompound() || stack.getTagCompound().getInteger("tiles") == 0)
 		{
