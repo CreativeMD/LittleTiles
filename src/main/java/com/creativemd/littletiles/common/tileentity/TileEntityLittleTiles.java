57ed2b86a5b0f525b9ca523b430f309736563561
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-03-07 17:25:49 +0100

    Fixed weird rendering issue for animated doors (if opened by another player)

--- src/main/java/com/creativemd/littletiles/common/tileentity/TileEntityLittleTiles.java
+++ src/main/java/com/creativemd/littletiles/common/tileentity/TileEntityLittleTiles.java
@@ -767,6 +767,12 @@ public class TileEntityLittleTiles extends TileEntityCreative implements ITickab
 	}
 	
 	@Override
+	public void onLoad()
+    {
+		setLoaded();
+    }
+	
+	@Override
 	public void update()
 	{
 		/*if(isClientSide())
@@ -778,7 +784,7 @@ public class TileEntityLittleTiles extends TileEntityCreative implements ITickab
 			}
 		}*/
 		
-		setLoaded();
+		
 		
 		for (Iterator iterator = updateTiles.iterator(); iterator.hasNext();) {
 			LittleTile tile = (LittleTile) iterator.next();
