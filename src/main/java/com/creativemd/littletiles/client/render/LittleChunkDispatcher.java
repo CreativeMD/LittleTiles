2d9f8c07354b7bb92cb00f3ca3a1db1a1af2b956
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-04-15 20:34:45 +0200

    Redesigned tileentities cached in RenderChunk

--- src/main/java/com/creativemd/littletiles/client/render/LittleChunkDispatcher.java
+++ src/main/java/com/creativemd/littletiles/client/render/LittleChunkDispatcher.java
@@ -8,6 +8,8 @@ import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Queue;
@@ -17,6 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import com.creativemd.littletiles.client.LittleTilesClient;
 import com.creativemd.littletiles.client.render.optifine.OptifineVertexBuffer;
 import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListenableFutureTask;
@@ -68,38 +71,70 @@ public class LittleChunkDispatcher extends ChunkRenderDispatcher {
 	
 	private static Field setTileEntities = ReflectionHelper.findField(RenderChunk.class, "setTileEntities", "field_181056_j");
 	
+	private static Field littleTiles = ReflectionHelper.findField(RenderChunk.class, "littleTiles");
+	
 	private static Minecraft mc = Minecraft.getMinecraft();
+	
+	public static void onStartRendering(RenderChunk chunk)
+	{
+		List<TileEntityLittleTiles> tiles = getLittleTE(chunk);
+		if(tiles == null)
+		{
+			tiles = new ArrayList<>();
+			try {
+				littleTiles.set(chunk, tiles);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		tiles.clear();
+	}
+	
+	public static void addTileEntity(RenderChunk chunk, TileEntity te)
+	{
+		if(te instanceof TileEntityLittleTiles)
+		{
+			getLittleTE(chunk).add((TileEntityLittleTiles) te);
+		}
+	}
+	
+	public static List<TileEntityLittleTiles> getLittleTE(RenderChunk chunk)
+	{
+		try {
+			return (List<TileEntityLittleTiles>) littleTiles.get(chunk);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	@Override
 	public ListenableFuture<Object> uploadChunk(final BlockRenderLayer layer, final VertexBuffer buffer, final RenderChunk chunk, final CompiledChunk compiled, final double p_188245_5_)
-    {
-		Set<TileEntity> tileEntities = null;
-		try{
-			tileEntities = (Set<TileEntity>) setTileEntities.get(chunk);
-		}catch(IllegalAccessException e){
+    {		
+		List<TileEntityLittleTiles> tiles = getLittleTE(chunk);
+		/*try{
+			tileEntities = compiled.getTileEntities();
+		}catch(Exception e){
 			e.printStackTrace();
 		}
+		List<TileEntityLittleTiles> tiles = new ArrayList<>();*/
+		if(tiles == null)
+			tiles = new ArrayList<>();
 		int bufferExpand = 0;
-		List<TileEntityLittleTiles> tiles = new ArrayList<>();
-		for (Iterator<TileEntity> iterator = tileEntities.iterator(); iterator.hasNext();) {
-			TileEntity te = iterator.next();
+		for (Iterator<TileEntityLittleTiles> iterator = tiles.iterator(); iterator.hasNext();) {
+			TileEntityLittleTiles te = iterator.next();
+			if(layer == BlockRenderLayer.SOLID)
+				((TileEntityLittleTiles) te).updateQuadCache(chunk);
 			
-			if(te instanceof TileEntityLittleTiles)
+			BlockLayerRenderBuffer blockLayerBuffer = ((TileEntityLittleTiles) te).getBuffer();
+			if(blockLayerBuffer != null)
 			{
-				if(layer == BlockRenderLayer.SOLID)
-					((TileEntityLittleTiles) te).updateQuadCache(chunk);
-				
-				BlockLayerRenderBuffer blockLayerBuffer = ((TileEntityLittleTiles) te).getBuffer();
-				if(blockLayerBuffer != null)
+				VertexBuffer teBuffer = blockLayerBuffer.getBufferByLayer(layer);
+				if(teBuffer != null && (layer != BlockRenderLayer.TRANSLUCENT || !((TileEntityLittleTiles) te).getBeenAddedToBuffer().get()))
 				{
-					VertexBuffer teBuffer = blockLayerBuffer.getBufferByLayer(layer);
-					if(teBuffer != null && (layer != BlockRenderLayer.TRANSLUCENT || !((TileEntityLittleTiles) te).getBeenAddedToBuffer().get()))
-					{
-						bufferExpand += teBuffer.getByteBuffer().limit();
-						tiles.add((TileEntityLittleTiles) te);
-						if(layer == BlockRenderLayer.TRANSLUCENT)
-							((TileEntityLittleTiles) te).getBeenAddedToBuffer().set(true);
-					}
+					bufferExpand += teBuffer.getByteBuffer().limit();
+					if(layer == BlockRenderLayer.TRANSLUCENT)
+						((TileEntityLittleTiles) te).getBeenAddedToBuffer().set(true);
 				}
 			}
 		}
@@ -119,9 +154,11 @@ public class LittleChunkDispatcher extends ChunkRenderDispatcher {
 			
 			if(buffer.getVertexFormat() != null && !(compiled.getState() instanceof LittleVertexBufferState))
 			{				
-				for (int i = 0; i < tiles.size(); i++) {
-					TileEntityLittleTiles te = tiles.get(i);
+				for (Iterator<TileEntityLittleTiles> iterator = tiles.iterator(); iterator.hasNext();) {
+					TileEntityLittleTiles te = iterator.next();
 					BlockLayerRenderBuffer blockLayerBuffer = ((TileEntityLittleTiles) te).getBuffer();
+					if(blockLayerBuffer == null)
+						continue;
 					VertexBuffer teBuffer = blockLayerBuffer.getBufferByLayer(layer);
 					if(teBuffer != null)
 					{
