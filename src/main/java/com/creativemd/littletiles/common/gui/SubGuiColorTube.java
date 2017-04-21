f689776970eee558357a9159c4bcff156f70a06c
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-04-21 16:54:45 +0200

    Enhanced color picker (suggested by starg09)

--- src/main/java/com/creativemd/littletiles/common/gui/SubGuiColorTube.java
+++ src/main/java/com/creativemd/littletiles/common/gui/SubGuiColorTube.java
@@ -1,7 +1,10 @@
 package com.creativemd.littletiles.common.gui;
 
+import org.lwjgl.util.Color;
+
 import com.creativemd.creativecore.common.utils.ColorUtils;
 import com.creativemd.creativecore.gui.container.SubGui;
+import com.creativemd.creativecore.gui.controls.gui.GuiColorPicker;
 import com.creativemd.creativecore.gui.controls.gui.GuiColorPlate;
 import com.creativemd.creativecore.gui.controls.gui.GuiSteppedSlider;
 import com.creativemd.creativecore.gui.event.gui.GuiControlChangedEvent;
@@ -25,20 +28,17 @@ public class SubGuiColorTube extends SubGui{
 	
 	@Override
 	public void createControls() {
-		Vec3i color = ColorUtils.IntToRGB(ItemColorTube.getColor(stack));
-		controls.add(new GuiSteppedSlider("colorX", 5, 5, 100, 5, (int)color.getX(), 0, 255));
-		controls.add(new GuiSteppedSlider("colorY", 5, 15, 100, 5, (int)color.getY(), 0, 255));
-		controls.add(new GuiSteppedSlider("colorZ", 5, 25, 100, 5, (int)color.getZ(), 0, 255));
-		controls.add(new GuiColorPlate("plate", 118, 7, 20, 20, color));
+		Color color = ColorUtils.IntToRGBA(ItemColorTube.getColor(stack));
+		color.setAlpha(255);
+		controls.add(new GuiColorPicker("picker", 5, 5, color));
 	}
 	
 	@CustomEventSubscribe
 	public void onChange(GuiControlChangedEvent event)
 	{
-		GuiColorPlate plate = (GuiColorPlate) get("plate");
-		plate.setColor(new Vec3i((int) ((GuiSteppedSlider) get("colorX")).value, (int) ((GuiSteppedSlider) get("colorY")).value, (int) ((GuiSteppedSlider) get("colorZ")).value));
+		GuiColorPicker picker = (GuiColorPicker) get("picker");
 		NBTTagCompound nbt = new NBTTagCompound();
-		nbt.setInteger("color", ColorUtils.RGBAToInt(plate.getColor()));
+		nbt.setInteger("color", ColorUtils.RGBAToInt(picker.color));
 		sendPacketToServer(nbt);
 	}
 
