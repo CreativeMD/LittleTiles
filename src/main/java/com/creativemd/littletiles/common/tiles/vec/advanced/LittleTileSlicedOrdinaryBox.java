23fd70cce5b8c9d440734a101f09ffd42398ac42
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-10-23 23:24:36 +0200

    Fixed undo not working properly for slices

--- src/main/java/com/creativemd/littletiles/common/tiles/vec/advanced/LittleTileSlicedOrdinaryBox.java
+++ src/main/java/com/creativemd/littletiles/common/tiles/vec/advanced/LittleTileSlicedOrdinaryBox.java
@@ -362,7 +362,7 @@ public class LittleTileSlicedOrdinaryBox extends LittleTileBox {
 			int sizeTwo = getSize(two);
 			
 			double diff = difOne / sizeOne + difTwo / sizeTwo;
-			return sizeOne >= difOne && sizeTwo >= difTwo && diff <= 1;
+			return sizeOne >= difOne && sizeTwo >= difTwo && (diff < 1 || LittleUtils.equals(diff, 1));
 		}
 		return false;
 	}
