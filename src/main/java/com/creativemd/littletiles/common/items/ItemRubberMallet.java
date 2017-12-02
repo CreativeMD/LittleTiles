45dc05f022e5a29dcb1ceaa0cf9b578b8db4731d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-12-02 19:04:49 +0100

    Removed RubberMallet from CreativeTab

--- src/main/java/com/creativemd/littletiles/common/items/ItemRubberMallet.java
+++ src/main/java/com/creativemd/littletiles/common/items/ItemRubberMallet.java
@@ -10,12 +10,14 @@ import com.creativemd.littletiles.LittleTiles;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -28,6 +30,12 @@ public class ItemRubberMallet extends Item {
 		setCreativeTab(LittleTiles.littleTab);
 		hasSubtypes = true;
 		setMaxStackSize(1);
+	}
+	
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+	{
+		
 	}
 	
 	@Override
