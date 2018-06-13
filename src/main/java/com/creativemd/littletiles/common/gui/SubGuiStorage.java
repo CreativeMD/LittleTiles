b16a36da00ce928d7af9974e56220c88b661b080
CreativeMD <Creative-Multimedia-Design@gmx.de>
2018-06-13 20:02:44 +0200

    Enhanced storage gui (scales with the number of slots) suggested by MineCrak

--- src/main/java/com/creativemd/littletiles/common/gui/SubGuiStorage.java
+++ src/main/java/com/creativemd/littletiles/common/gui/SubGuiStorage.java
@@ -4,28 +4,43 @@ import com.creativemd.creativecore.gui.ContainerControl;
 import com.creativemd.creativecore.gui.container.SubGui;
 import com.creativemd.creativecore.gui.controls.container.SlotControl;
 import com.creativemd.creativecore.gui.controls.gui.GuiScrollBox;
+import com.creativemd.littletiles.common.container.SubContainerStorage.StorageSize;
 import com.creativemd.littletiles.common.structure.LittleStorage;
+
+import net.minecraft.inventory.IInventory;
 
 public class SubGuiStorage extends SubGui{
 	
 	public LittleStorage storage;
+	public final StorageSize size;
 	
 	public SubGuiStorage(LittleStorage storage) {
 		super(250, 250);
+		this.size = StorageSize.getSizeFromInventory(storage.inventory);
 		this.storage = storage;
+		setDimension(size.width, size.height);
 	}
 	
 	@Override
 	public void addContainerControls()
 	{
-		GuiScrollBox box = new GuiScrollBox("box", 0, 0, 245, 150);
+		if(!size.scrollbox)
+		{
+			super.addContainerControls();
+			return ;
+		}
+		
+		GuiScrollBox box = new GuiScrollBox("box", 0, 0, 244, 150);
 		controls.add(box);
 		for (int i = 0; i < container.controls.size(); i++) {
 			ContainerControl control = container.controls.get(i);
 			control.onOpened();
 			
 			if(control instanceof SlotControl && ((SlotControl) control).slot.inventory == storage.inventory)
+			{
+				((SlotControl) control).slot.xPos -= 4;
 				box.addControl(control.getGuiControl());
+			}
 			else
 				controls.add(control.getGuiControl());
 		}
