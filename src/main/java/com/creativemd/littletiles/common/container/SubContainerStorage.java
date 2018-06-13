8faf414b34824198839c2ef669b0d0ebfd84df01
CreativeMD <Creative-Multimedia-Design@gmx.de>
2018-06-13 20:02:44 +0200

    Enhanced storage gui (scales with the number of slots) suggested by MineCrak

--- src/main/java/com/creativemd/littletiles/common/container/SubContainerStorage.java
+++ src/main/java/com/creativemd/littletiles/common/container/SubContainerStorage.java
@@ -3,36 +3,73 @@ package com.creativemd.littletiles.common.container;
 import com.creativemd.creativecore.gui.container.SubContainer;
 import com.creativemd.creativecore.slots.SlotStackLimit;
 import com.creativemd.littletiles.common.structure.LittleStorage;
-import com.creativemd.littletiles.common.tiles.LittleTile;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class SubContainerStorage extends SubContainer {
 	
+	public static enum StorageSize {
+		
+		SMALL(176, 166, 8, 84, false),
+		Large(250, 250, 45, 170, false),
+		INFINITE(250, 250, 45, 170, true);
+		
+		public final int height;
+		public final int width;
+		public final int playerOffsetX;
+		public final int playerOffsetY;
+		public final boolean scrollbox;
+		
+		StorageSize(int width, int height, int playerOffsetX, int playerOffsetY, boolean scrollbox)
+		{
+			this.scrollbox = scrollbox;
+			this.height = height;
+			this.width = width;
+			this.playerOffsetX = playerOffsetX;
+			this.playerOffsetY = playerOffsetY;
+		}
+		
+		public static StorageSize getSizeFromInventory(IInventory inventory)
+		{
+			if(inventory.getSizeInventory() <= 27)
+				return StorageSize.SMALL;
+			else if(inventory.getSizeInventory() <= 117)
+				return Large;
+			return StorageSize.INFINITE;
+		}
+		
+	}
+
 	public LittleStorage storage;
+	public final StorageSize size;
 
 	public SubContainerStorage(EntityPlayer player, LittleStorage storage) {
 		super(player);
 		this.storage = storage;
+		this.size = StorageSize.getSizeFromInventory(storage.inventory);
 	}
 
 	@Override
 	public void createControls() {
 		if(storage.inventory != null)
 		{
-			int slotsPerRow = 12;
+			int slotsPerRow = size.width/18;
+			int rows = (int) Math.ceil(storage.inventory.getSizeInventory()/(double)slotsPerRow);
+			int rowWidth = Math.min(slotsPerRow, storage.inventory.getSizeInventory()) * 18;
+			int offsetX = (size.width - rowWidth) / 2;
+			
 			for (int i = 0; i < storage.inventory.getSizeInventory(); i++) {
 				int row = i/slotsPerRow;
 				int rowIndex = i-row*slotsPerRow;
 				int stackSize = storage.stackSizeLimit;
 				if(i+1 == storage.numberOfSlots && storage.lastSlotStackSize > 0)
 					stackSize = storage.lastSlotStackSize;
-				addSlotToContainer(new SlotStackLimit(storage.inventory, i, 5+rowIndex*18, 5+row*18, stackSize));
+				addSlotToContainer(new SlotStackLimit(storage.inventory, i, offsetX+rowIndex*18, 5+row*18, stackSize));
 			}
 			
-			addPlayerSlotsToContainer(player, 45, 170);
+			addPlayerSlotsToContainer(player, size.playerOffsetX, size.playerOffsetY);
 		}
 	}
 
