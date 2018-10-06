a311036015fd0e72ab01237a22bb8de7a2a1f338
CreativeMD <Creative-Multimedia-Design@gmx.de>
2018-10-06 21:06:11 +0200

    Fixed crash on server side

--- src/main/java/com/creativemd/littletiles/LittleTiles.java
+++ src/main/java/com/creativemd/littletiles/LittleTiles.java
@@ -434,6 +434,7 @@ public class LittleTiles {
 		GuiHandler.registerGuiHandler("structureoverview", new LittleGuiHandler() {
 			
 			@Override
+			@SideOnly(Side.CLIENT)
 			public SubGui getGui(EntityPlayer player, NBTTagCompound nbt, LittleTile tile) {
 				return new SubGuiStructureOverview(tile);
 			}
