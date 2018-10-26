070c67dd29da93e51a68fab8bbe96f081d84ba1e
CreativeMD <Creative-Multimedia-Design@gmx.de>
2018-10-26 13:28:23 +0200

    Added the ability to use the advanced recipe to craft structures in workbench

--- src/main/java/com/creativemd/littletiles/common/container/SubContainerWorkbench.java
+++ src/main/java/com/creativemd/littletiles/common/container/SubContainerWorkbench.java
@@ -7,6 +7,7 @@ import com.creativemd.littletiles.common.action.LittleAction;
 import com.creativemd.littletiles.common.action.block.NotEnoughIngredientsException;
 import com.creativemd.littletiles.common.api.ILittleTile;
 import com.creativemd.littletiles.common.items.ItemRecipe;
+import com.creativemd.littletiles.common.items.ItemRecipeAdvanced;
 import com.creativemd.littletiles.common.mods.chiselsandbits.ChiselsAndBitsManager;
 import com.creativemd.littletiles.common.tiles.preview.LittlePreviews;
 import com.creativemd.littletiles.common.tiles.preview.LittleTilePreview;
@@ -34,28 +35,6 @@ public class SubContainerWorkbench extends SubContainer {
 		addPlayerSlotsToContainer(player);
 	}
 	
-	/* public static ArrayList<BlockEntry> getMissing(ArrayList<LittleTilePreview>
-	 * tiles, ArrayList<BlockEntry> entries) { ArrayList<BlockEntry> missing = new
-	 * ArrayList<>(); for (int i = 0; i < tiles.size(); i++) { Block blockofTile =
-	 * tiles.get(i).getPreviewBlock(); int meta =
-	 * tiles.get(i).getPreviewBlockMeta(); double size =
-	 * tiles.get(i).size.getPercentVolume(); int j = 0; boolean found = false;
-	 * while(j < entries.size()) { if(blockofTile == entries.get(j).block && meta ==
-	 * entries.get(j).meta) { double amount = Math.min(entries.get(j).value, size);
-	 * entries.get(j).value -= amount; size -= amount;
-	 * 
-	 * if(entries.get(j).value <= 0)
-	 * {
-	 * entries.remove(j);
-	 * if(size > 0)
-	 * continue;
-	 * }
-	 * 
-	 * if(size <= 0) { found = true; break; } } j++; } if(!found) { BlockEntry entry
-	 * = new BlockEntry(blockofTile, meta, size); int index =
-	 * missing.indexOf(entry); if(index == -1) missing.add(entry); else
-	 * missing.get(index).value += size; } } return missing; } */
-	
 	@Override
 	public void onClosed() {
 		for (int i = 0; i < basic.getSizeInventory(); i++) {
@@ -69,7 +48,7 @@ public class SubContainerWorkbench extends SubContainer {
 		ItemStack stack1 = basic.getStackInSlot(0);
 		ItemStack stack2 = basic.getStackInSlot(1);
 		if (!stack1.isEmpty()) {
-			if (stack1.getItem() instanceof ItemRecipe) {
+			if (stack1.getItem() instanceof ItemRecipe || stack1.getItem() instanceof ItemRecipeAdvanced) {
 				if (stack1.getTagCompound() != null && !stack1.getTagCompound().hasKey("x")) {
 					LittlePreviews tiles = LittleTilePreview.getPreview(stack1);
 					
