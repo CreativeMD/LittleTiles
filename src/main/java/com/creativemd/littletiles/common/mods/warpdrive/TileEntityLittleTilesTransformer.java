5009621825660de860a6d73f92504fa8f7c34001
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-06-20 17:38:56 +0200

    Finished support for WarpDrive

--- src/main/java/com/creativemd/littletiles/common/mods/warpdrive/TileEntityLittleTilesTransformer.java
+++ src/main/java/com/creativemd/littletiles/common/mods/warpdrive/TileEntityLittleTilesTransformer.java
@@ -2,12 +2,13 @@ package com.creativemd.littletiles.common.mods.warpdrive;
 
 import java.lang.reflect.InvocationTargetException;
 
+import com.creativemd.creativecore.common.utils.math.Rotation;
+import com.creativemd.creativecore.common.utils.math.RotationUtils;
 import com.creativemd.littletiles.LittleTiles;
 import com.creativemd.littletiles.common.structure.connection.IStructureConnector;
 import com.creativemd.littletiles.common.structure.connection.StructureLinkBaseRelative;
 import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
 import com.creativemd.littletiles.common.tiles.LittleTile;
-import com.creativemd.littletiles.common.tiles.vec.LittleTileBox;
 import com.creativemd.littletiles.common.tiles.vec.LittleTileIdentifierStructureAbsolute;
 
 import cr0s.warpdrive.api.IBlockTransformer;
@@ -19,7 +20,7 @@ import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 
 public class TileEntityLittleTilesTransformer implements IBlockTransformer {
@@ -60,12 +61,12 @@ public class TileEntityLittleTilesTransformer implements IBlockTransformer {
 		if (rotationSteps == 0) {
 			return metadata;
 		}
+		
 		TileEntityLittleTiles te = new TileEntityLittleTiles();
 		te.readFromNBT(nbtTileEntity);
-		for (LittleTile tile : te.getTiles()) {
+		for (LittleTile tile : te.getTiles())
 			for (int rotationStep = 0; rotationStep < rotationSteps; rotationStep++)
-				transformTile(tile, transformation);
-		}
+				transformTile(tile, Rotation.Y_CLOCKWISE);
 		te.writeToNBT(nbtTileEntity);
 		return metadata;
 	}
@@ -75,38 +76,27 @@ public class TileEntityLittleTilesTransformer implements IBlockTransformer {
 		
 	}
 	
-	public static void transformTile(LittleTile tile, ITransformation transformation) {
-		transformBox(tile.box, transformation);
+	public static void transformTile(LittleTile tile, Rotation rotation) {
+		tile.box.rotateBox(rotation, tile.getContext().rotationCenter);
 		if (tile.connection != null)
-			transformConnection(tile.connection, transformation);
+			transformConnection(tile.connection, rotation);
 	}
 	
-	public static void transformBox(LittleTileBox box, ITransformation transformation) {
-		Vec3d min = transformation.apply((double) box.minX, (double) box.minY, (double) box.minZ);
-		Vec3d max = transformation.apply((double) box.maxX, (double) box.maxY, (double) box.maxZ);
-		box.minX = (int) Math.min(min.x, max.x);
-		box.minY = (int) Math.min(min.y, max.y);
-		box.minZ = (int) Math.min(min.z, max.z);
-		box.maxX = (int) Math.max(min.x, max.x);
-		box.maxY = (int) Math.max(min.y, max.y);
-		box.maxZ = (int) Math.max(min.z, max.z);
-	}
-	
-	public static void transformConnection(IStructureConnector<LittleTile> connector, ITransformation transformation) {
+	public static void transformConnection(IStructureConnector<LittleTile> connector, Rotation rotation) {
 		if (connector instanceof StructureLinkBaseRelative) {
 			StructureLinkBaseRelative connect = (StructureLinkBaseRelative) connector;
-			connect.coord = transformation.apply(connect.coord.getX(), connect.coord.getY(), connect.coord.getZ());
-			Vec3d vec = transformation.apply((double) connect.identifier[0], (double) connect.identifier[1], (double) connect.identifier[2]);
-			connect.identifier[0] = (int) vec.x;
-			connect.identifier[1] = (int) vec.y;
-			connect.identifier[2] = (int) vec.z;
+			connect.coord = new BlockPos(RotationUtils.rotate(connect.coord, rotation));
+			Vec3i vec = RotationUtils.rotate(new Vec3i((double) connect.identifier[0], (double) connect.identifier[1], (double) connect.identifier[2]), rotation);
+			connect.identifier[0] = (int) vec.getX();
+			connect.identifier[1] = (int) vec.getY();
+			connect.identifier[2] = (int) vec.getZ();
 		} else if (connector instanceof LittleTileIdentifierStructureAbsolute) {
 			LittleTileIdentifierStructureAbsolute connect = (LittleTileIdentifierStructureAbsolute) connector;
-			connect.pos = transformation.apply(connect.pos.getX(), connect.pos.getY(), connect.pos.getZ());
-			Vec3d vec = transformation.apply((double) connect.identifier[0], (double) connect.identifier[1], (double) connect.identifier[2]);
-			connect.identifier[0] = (int) vec.x;
-			connect.identifier[1] = (int) vec.y;
-			connect.identifier[2] = (int) vec.z;
+			connect.pos = new BlockPos(RotationUtils.rotate(connect.pos, rotation));
+			Vec3i vec = RotationUtils.rotate(new Vec3i((double) connect.identifier[0], (double) connect.identifier[1], (double) connect.identifier[2]), rotation);
+			connect.identifier[0] = (int) vec.getX();
+			connect.identifier[1] = (int) vec.getY();
+			connect.identifier[2] = (int) vec.getZ();
 		}
 	}
 }
