2c343336c341126da59993c3a91814864acb871f
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-07-06 14:42:28 +0200

    Added new /lt-tovanilla command

--- src/main/java/com/creativemd/littletiles/LittleTiles.java
+++ src/main/java/com/creativemd/littletiles/LittleTiles.java
@@ -48,6 +48,7 @@ import com.creativemd.littletiles.common.blocks.ItemBlockTransparentColored;
 import com.creativemd.littletiles.common.command.ExportCommand;
 import com.creativemd.littletiles.common.command.ImportCommand;
 import com.creativemd.littletiles.common.command.OpenCommand;
+import com.creativemd.littletiles.common.command.ToVanillaCommand;
 import com.creativemd.littletiles.common.config.IGCMLoader;
 import com.creativemd.littletiles.common.container.SubContainerDiagnose;
 import com.creativemd.littletiles.common.container.SubContainerExport;
@@ -540,5 +541,6 @@ public class LittleTiles {
 		event.registerServerCommand(new ExportCommand());
 		event.registerServerCommand(new ImportCommand());
 		event.registerServerCommand(new OpenCommand());
+		event.registerServerCommand(new ToVanillaCommand());
 	}
 }
---
+++ src/main/java/com/creativemd/littletiles/common/command/ToVanillaCommand.java
@@ -0,0 +1,47 @@
+package com.creativemd.littletiles.common.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class ToVanillaCommand extends CommandBase {
+	
+	@Override
+	public String getName() {
+		return "lt-tovanilla";
+	}
+	
+	@Override
+	public String getUsage(ICommandSender sender) {
+		return "commands.tovanilla.usage";
+	}
+	
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		World world = sender.getEntityWorld();
+		List<TileEntityLittleTiles> blocks = new ArrayList<>();
+		for (TileEntity te : world.loadedTileEntityList)
+			if (te instanceof TileEntityLittleTiles)
+				blocks.add((TileEntityLittleTiles) te);
+		System.out.println("Attempting to convert " + blocks.size() + " blocks!");
+		int converted = 0;
+		int i = 0;
+		for (TileEntityLittleTiles te : blocks) {
+			if (te.convertBlockToVanilla())
+				converted++;
+			i++;
+			if (i % 50 == 0)
+				System.out.println("Processed " + i + "/" + blocks.size() + " and converted " + converted);
+		}
+		System.out.println("Converted " + converted + " blocks");
+	}
+	
+}
