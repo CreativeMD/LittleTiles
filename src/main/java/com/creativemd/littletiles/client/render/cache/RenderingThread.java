f4d9823be36f88e6e00733471ae609197d23661c
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-12-13 15:14:41 +0100

    Fixed clear waiting animation being done too early (caused some visual flickering)

--- src/main/java/com/creativemd/littletiles/client/render/cache/RenderingThread.java
+++ src/main/java/com/creativemd/littletiles/client/render/cache/RenderingThread.java
@@ -379,7 +379,6 @@ public class RenderingThread extends Thread {
 		te.lastRenderedChunk = null;
 		te.inRenderingQueue.set(false);
 		te.buildingCache.set(false);
-		te.clearWaitingAnimations();
 		
 		synchronized (chunks) {
 			AtomicInteger count = chunks.get(data.chunk);
