979b00caba111d6109fdb428059c281f6c100a74
CreativeMD <Creative-Multimedia-Design@gmx.de>
2020-03-08 18:30:17 +0100

    Added temporary fix for dynamicsurroundings

---
+++ src/main/java/com/creativemd/littletiles/common/blocks/BlockTile.java
@@ -0,0 +1,13 @@
+package com.creativemd.littletiles.common.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockTile extends Block {
+	
+	public BlockTile(Material materialIn) {
+		super(materialIn);
+		
+	}
+	
+}
