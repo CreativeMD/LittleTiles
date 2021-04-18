d6cc88d1b6abb13667e14c233ef278b3bfa2cdcc
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-04-18 21:19:08 +0200

    Fixed crash on server side

--- src/main/java/com/creativemd/littletiles/common/util/tooltip/IItemTooltip.java
+++ src/main/java/com/creativemd/littletiles/common/util/tooltip/IItemTooltip.java
@@ -1,10 +1,7 @@
 package com.creativemd.littletiles.common.util.tooltip;
 
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public interface IItemTooltip {
     
     public Object[] tooltipData(ItemStack stack);
