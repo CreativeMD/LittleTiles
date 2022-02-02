e4afcdc53e534bbfab8ece661a577dd706b0639d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-02-02 13:23:56 +0100

    Added LittleExportType Fixed GuiExport

--- src/main/java/team/creative/littletiles/common/gui/premade/GuiExport.java
+++ src/main/java/team/creative/littletiles/common/gui/premade/GuiExport.java
@@ -4,55 +4,59 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 
-import com.creativemd.creativecore.common.gui.event.container.SlotChangeEvent;
-import com.creativemd.littletiles.common.util.converation.StructureStringUtils;
-import com.n247s.api.eventapi.eventsystem.CustomEventSubscribe;
-
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import team.creative.creativecore.common.gui.GuiLayer;
-import team.creative.creativecore.common.gui.controls.collection.GuiComboBox;
+import team.creative.creativecore.common.gui.GuiParent;
+import team.creative.creativecore.common.gui.controls.collection.GuiComboBoxMapped;
+import team.creative.creativecore.common.gui.controls.inventory.GuiInventoryGrid;
+import team.creative.creativecore.common.gui.controls.inventory.GuiPlayerInventoryGrid;
 import team.creative.creativecore.common.gui.controls.simple.GuiButton;
 import team.creative.creativecore.common.gui.controls.simple.GuiTextfield;
-import team.creative.creativecore.common.util.text.TextListBuilder;
-import team.creative.littletiles.common.item.ItemLittleBlueprint;
+import team.creative.creativecore.common.gui.flow.GuiFlow;
+import team.creative.creativecore.common.util.text.TextMapBuilder;
+import team.creative.littletiles.client.export.LittleExportType;
 import team.creative.littletiles.common.placement.PlacementHelper;
 
 public class GuiExport extends GuiLayer {
     
     public GuiTextfield textfield;
+    public Container exportSlot = new SimpleContainer(1);
+    
+    public GuiExport() {
+        super("gui.little.export");
+    }
     
     @Override
     public void create() {
+        add(new GuiInventoryGrid("export", exportSlot));
+        
+        GuiParent row = new GuiParent(GuiFlow.STACK_X);
+        row.setExpandableX();
+        add(row);
         textfield = new GuiTextfield("export");
         textfield.setMaxStringLength(Integer.MAX_VALUE);
-        add(textfield);
+        row.add(textfield);
         
-        add(new GuiButton("Copy", x -> {
+        row.add(new GuiButton("Copy", x -> {
             StringSelection stringSelection = new StringSelection(textfield.getText());
             Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
             clpbrd.setContents(stringSelection, null);
         }));
         
-        add(new GuiComboBox("type", new TextListBuilder().addTranslated("gui.export.", "structure", "model")));
+        add(new GuiComboBoxMapped<LittleExportType>("type", new TextMapBuilder<LittleExportType>()
+                .addEntrySet(LittleExportType.REGISTRY.entrySet(), x -> new TranslatableComponent("gui.export." + x.getKey()))));
         
-        registerEventChanged(x -> updateTextfield());
-    }
-    
-    public void updateTextfield() {
-        ItemStack stack = ((GuiExport) container).slot.getStackInSlot(0);
-        if (stack != null && (PlacementHelper.isLittleBlock(stack) || stack.getItem() instanceof ItemLittleBlueprint)) {
-            GuiComboBox box = (GuiComboBox) get("type");
-            if (box.getIndex() == 0)
-                textfield.setText(StructureStringUtils.exportStructure(stack));
+        add(new GuiPlayerInventoryGrid(getPlayer()));
+        
+        registerEventChanged(x -> {
+            ItemStack stack = exportSlot.getItem(0);
+            if (!stack.isEmpty() && PlacementHelper.isLittleBlock(stack))
+                textfield.setText(((GuiComboBoxMapped<LittleExportType>) get("type")).getSelected().export(stack));
             else
-                textfield.setText(StructureStringUtils.exportModel(stack));
-        } else
-            textfield.setText("");
+                textfield.setText("");
+        });
     }
-    
-    @CustomEventSubscribe
-    public void onSlotChange(SlotChangeEvent event) {
-        updateTextfield();
-    }
-    
 }
