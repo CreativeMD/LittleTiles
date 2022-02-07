8fb875a0ff270c461eef64b8dfdc43a3aee2f7b2
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-02-07 12:53:36 +0100

    Added color to GuiColorProgressBar

--- src/main/java/team/creative/littletiles/common/gui/controls/GuiColorProgressBar.java
+++ src/main/java/team/creative/littletiles/common/gui/controls/GuiColorProgressBar.java
@@ -2,16 +2,30 @@ package team.creative.littletiles.common.gui.controls;
 
 import java.util.List;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.sounds.SoundEvents;
+import team.creative.creativecore.client.render.GuiRenderHelper;
+import team.creative.creativecore.common.gui.GuiChildControl;
 import team.creative.creativecore.common.gui.controls.simple.GuiProgressbar;
+import team.creative.creativecore.common.gui.style.ControlFormatting;
 import team.creative.creativecore.common.util.math.geo.Rect;
+import team.creative.creativecore.common.util.type.Color;
 
 public class GuiColorProgressBar extends GuiProgressbar {
     
-    public GuiColorProgressBar(String name, double max, double pos) {
+    public Color color;
+    
+    public GuiColorProgressBar(String name, double max, double pos, Color color) {
         super(name, max, pos);
+        this.color = color;
+    }
+    
+    @Override
+    public ControlFormatting getControlFormatting() {
+        return ControlFormatting.NESTED_NO_PADDING;
     }
     
     @Override
@@ -29,6 +43,12 @@ public class GuiColorProgressBar extends GuiProgressbar {
             return true;
         }
         return false;
+    }
+    
+    @Override
+    protected void renderContent(PoseStack pose, GuiChildControl control, Rect rect, int mouseX, int mouseY) {
+        GuiRenderHelper.colorRect(pose, 0, 0, (int) rect.getWidth(), (int) rect.getHeight(), color.toInt());
+        super.renderContent(pose, control, rect, mouseX, mouseY);
     }
     
 }
