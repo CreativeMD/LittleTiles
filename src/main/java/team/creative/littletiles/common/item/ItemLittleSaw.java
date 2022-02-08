e851848a9bb24ad1f7754d6f4e4322cb3c3b50bc
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-02-08 14:37:57 +0100

    Removed saw functionality

--- src/main/java/team/creative/littletiles/common/item/ItemLittleSaw.java
+++ src/main/java/team/creative/littletiles/common/item/ItemLittleSaw.java
@@ -1,17 +1,11 @@
 package team.creative.littletiles.common.item;
 
-import com.creativemd.littletiles.common.action.tool.LittleActionSaw;
-import com.creativemd.littletiles.common.util.grid.LittleGridContext;
-
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import team.creative.littletiles.LittleTiles;
-import team.creative.littletiles.common.block.entity.BETiles;
 import team.creative.littletiles.common.item.tooltip.IItemTooltip;
 
 public class ItemLittleSaw extends Item implements IItemTooltip {
@@ -22,12 +16,6 @@ public class ItemLittleSaw extends Item implements IItemTooltip {
     
     @Override
     public InteractionResult useOn(UseOnContext context) {
-        BlockEntity blockEntity = context.getLevel().getBlockEntity(context.getClickedPos());
-        if (blockEntity instanceof BETiles) {
-            if (context.getLevel().isClientSide)
-                new LittleActionSaw(context.getLevel(), context.getClickedPos(), context.getClickedFace(), GuiScreen.isCtrlKeyDown(), LittleGridContext.get()).execute();
-            return InteractionResult.SUCCESS;
-        }
         return InteractionResult.PASS;
     }
     
