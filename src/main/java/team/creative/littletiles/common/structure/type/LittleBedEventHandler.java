134043136667b3cedbdfcd66fd6a7f17e97ea406
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-02-08 15:49:50 +0100

    Fixed BedEventHandler

--- src/main/java/team/creative/littletiles/common/structure/type/LittleBedEventHandler.java
+++ src/main/java/team/creative/littletiles/common/structure/type/LittleBedEventHandler.java
@@ -1,28 +1,29 @@
 package team.creative.littletiles.common.structure.type;
 
-import org.spongepowered.asm.mixin.MixinEnvironment.Side;
-
-import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
-
-import net.minecraft.entity.player.EntityPlayer.SleepResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Player.BedSleepingProblem;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedOutEvent;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.event.entity.player.SleepingLocationCheckEvent;
+import net.minecraftforge.eventbus.api.Event.Result;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import team.creative.littletiles.common.block.entity.BETiles;
 import team.creative.littletiles.common.block.mc.BlockTile;
 import team.creative.littletiles.common.structure.LittleStructure;
 import team.creative.littletiles.common.structure.exception.CorruptedConnectionException;
+import team.creative.littletiles.common.structure.exception.NotYetConnectedException;
 
 public class LittleBedEventHandler {
     
     @SubscribeEvent
     public void isSleepingLocationAllowed(SleepingLocationCheckEvent event) {
         try {
-            LittleStructure bed = (LittleStructure) LittleBed.littleBed.get(event.getEntityPlayer());
-            if (bed instanceof LittleBed && ((LittleBed) bed).getSleepingPlayer() == event.getEntityPlayer())
-                event.setResult(Result.ALLOW);
+            if (event.getEntityLiving() instanceof Player player) {
+                LittleStructure bed = (LittleStructure) LittleBed.littleBed.get(player);
+                if (bed instanceof LittleBed && ((LittleBed) bed).getSleepingPlayer() == player)
+                    event.setResult(Result.ALLOW);
+            }
         } catch (IllegalArgumentException | IllegalAccessException e) {
             e.printStackTrace();
         }
@@ -31,10 +32,10 @@ public class LittleBedEventHandler {
     @SubscribeEvent
     public void onPlayerLogout(PlayerLoggedOutEvent event) {
         try {
-            LittleStructure bed = (LittleStructure) LittleBed.littleBed.get(event.player);
+            LittleStructure bed = (LittleStructure) LittleBed.littleBed.get(event.getPlayer());
             if (bed instanceof LittleBed)
                 ((LittleBed) bed).setSleepingPlayer(null);
-            LittleBed.littleBed.set(event.player, null);
+            LittleBed.littleBed.set(event.getPlayer(), null);
         } catch (IllegalArgumentException | IllegalAccessException e) {
             e.printStackTrace();
         }
@@ -43,26 +44,25 @@ public class LittleBedEventHandler {
     @SubscribeEvent
     public void onWakeUp(PlayerWakeUpEvent event) {
         try {
-            LittleStructure bed = (LittleStructure) LittleBed.littleBed.get(event.getEntityPlayer());
+            LittleStructure bed = (LittleStructure) LittleBed.littleBed.get(event.getPlayer());
             if (bed instanceof LittleBed)
                 ((LittleBed) bed).setSleepingPlayer(null);
-            LittleBed.littleBed.set(event.getEntityPlayer(), null);
+            LittleBed.littleBed.set(event.getPlayer(), null);
         } catch (IllegalArgumentException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
     
-    @SideOnly(Side.CLIENT)
     @SubscribeEvent
     public void onPlayerSleep(PlayerSleepInBedEvent event) {
-        if (event.getEntityPlayer().world.getBlockState(event.getPos()).getBlock() instanceof BlockTile) {
-            TileEntityLittleTiles te = BlockTile.loadTe(event.getEntityPlayer().world, event.getPos());
-            if (te != null) {
-                for (LittleStructure structure : te.loadedStructures()) {
+        if (event.getPlayer().level.getBlockState(event.getPos()).getBlock() instanceof BlockTile) {
+            BETiles be = BlockTile.loadBE(event.getPlayer().level, event.getPos());
+            if (be != null) {
+                for (LittleStructure structure : be.loadedStructures()) {
                     if (structure instanceof LittleBed && ((LittleBed) structure).hasBeenActivated) {
                         try {
-                            ((LittleBed) structure).trySleep(event.getEntityPlayer(), structure.getHighestCenterVec());
-                            event.setResult(SleepResult.OK);
+                            ((LittleBed) structure).trySleep(event.getPlayer(), structure.getHighestCenterVec());
+                            event.setResult((BedSleepingProblem) null);
                             ((LittleBed) structure).hasBeenActivated = false;
                             return;
                         } catch (CorruptedConnectionException | NotYetConnectedException e) {
