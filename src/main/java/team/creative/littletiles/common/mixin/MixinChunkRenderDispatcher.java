9ae142376b59828d56698bda04a9dacd6f316796
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-02-21 09:44:45 +0100

    Added ChunkRenderDispatcher hook

---
+++ src/main/java/team/creative/littletiles/common/mixin/MixinChunkRenderDispatcher.java
@@ -0,0 +1,24 @@
+package team.creative.littletiles.common.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.client.renderer.ChunkBufferBuilderPack;
+import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher.CompiledChunk;
+import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher.RenderChunk;
+import net.minecraft.world.phys.Vec3;
+import team.creative.littletiles.client.render.level.LittleChunkDispatcher;
+
+@Mixin(targets = "net.minecraft.client.renderer.chunk.ChunkRenderDispatcher.RenderChunk.RebuildTask")
+public class MixinChunkRenderDispatcher {
+    
+    @Inject(at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList()Ljava/util/ArrayList;"),
+            method = "doTask(Lnet/minecraft/client/renderer/ChunkBufferBuilderPack;)", locals = LocalCapture.CAPTURE_FAILHARD)
+    private void rebuild(ChunkBufferBuilderPack pack, CallbackInfo info, Vec3 vec, float x, float y, float z, CompiledChunk compiled) {
+        LittleChunkDispatcher.beforeUploadRebuild(pack, compiled, (RenderChunk) (Object) this);
+    }
+    
+}
