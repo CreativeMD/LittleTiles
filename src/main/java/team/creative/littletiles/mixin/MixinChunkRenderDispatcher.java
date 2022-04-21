8eea0fe33622e1959d75910d09fece7f703cfc29
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-04-21 17:15:27 +0200

    Fixed warning not finding mapping location

--- src/main/java/team/creative/littletiles/mixin/MixinChunkRenderDispatcher.java
+++ src/main/java/team/creative/littletiles/mixin/MixinChunkRenderDispatcher.java
@@ -15,7 +15,7 @@ import team.creative.littletiles.client.render.level.LittleChunkDispatcher;
 @Mixin(targets = "net.minecraft.client.renderer.chunk.ChunkRenderDispatcher.RenderChunk.RebuildTask")
 public abstract class MixinChunkRenderDispatcher {
     
-    @Inject(at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList()Ljava/util/ArrayList;"),
+    @Inject(at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList()Ljava/util/ArrayList;", remap = false),
             method = "doTask(Lnet/minecraft/client/renderer/ChunkBufferBuilderPack;)Ljava/util/concurrent/CompletableFuture;", locals = LocalCapture.CAPTURE_FAILHARD)
     private void rebuild(ChunkBufferBuilderPack pack, CallbackInfo info, Vec3 vec, float x, float y, float z, CompiledChunk compiled) {
         LittleChunkDispatcher.beforeUploadRebuild(pack, compiled, (RenderChunk) (Object) this);
