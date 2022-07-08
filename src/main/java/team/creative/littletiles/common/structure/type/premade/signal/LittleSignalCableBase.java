a2213ebe09d81ef8e5e11cd3bd85eadbd18e6555
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-07-08 12:15:53 +0200

    Fixed cable unload loop

--- src/main/java/team/creative/littletiles/common/structure/type/premade/signal/LittleSignalCableBase.java
+++ src/main/java/team/creative/littletiles/common/structure/type/premade/signal/LittleSignalCableBase.java
@@ -515,7 +515,7 @@ public abstract class LittleSignalCableBase extends LittleStructurePremade imple
     public void unload(Facing facing, ISignalStructureBase base) {
         int index = getIndex(facing);
         if (faces[index] != null)
-            faces[index].connection.unload(facing, base);
+            faces[index].connection = null;
     }
     
     public class LittleConnectionFace {
@@ -531,10 +531,11 @@ public abstract class LittleSignalCableBase extends LittleStructurePremade imple
         }
         
         public void disconnect(Facing facing) {
-            if (connection != null)
+            if (connection != null) {
                 connection.disconnect(facing.opposite(), LittleSignalCableBase.this);
-            if (hasNetwork())
-                getNetwork().remove(connection);
+                if (hasNetwork())
+                    getNetwork().remove(connection);
+            }
             connection = null;
             updateStructure();
         }
