20c556e8aca290620866b191f992a0075496fa9b
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-07-20 14:15:28 +0200

    More gui fixes and removals

--- src/main/java/team/creative/littletiles/common/gui/tool/GuiBag.java
+++ src/main/java/team/creative/littletiles/common/gui/tool/GuiBag.java
@@ -5,24 +5,31 @@ import java.util.List;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.EndTag;
 import net.minecraft.nbt.StringTag;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import team.creative.creativecore.common.gui.Align;
 import team.creative.creativecore.common.gui.GuiParent;
+import team.creative.creativecore.common.gui.controls.inventory.GuiInventoryGrid;
 import team.creative.creativecore.common.gui.controls.inventory.GuiPlayerInventoryGrid;
 import team.creative.creativecore.common.gui.flow.GuiFlow;
 import team.creative.creativecore.common.gui.sync.GuiSyncLocal;
 import team.creative.creativecore.common.util.inventory.ContainerSlotView;
 import team.creative.creativecore.common.util.mc.LevelUtils;
 import team.creative.creativecore.common.util.type.Color;
-import team.creative.littletiles.LittleTiles;
+import team.creative.littletiles.LittleTilesRegistry;
+import team.creative.littletiles.common.api.ingredient.ILittleIngredientInventory;
 import team.creative.littletiles.common.gui.configure.GuiConfigure;
 import team.creative.littletiles.common.gui.controls.GuiColorProgressBar;
 import team.creative.littletiles.common.ingredient.BlockIngredient;
 import team.creative.littletiles.common.ingredient.BlockIngredientEntry;
 import team.creative.littletiles.common.ingredient.ColorIngredient;
+import team.creative.littletiles.common.ingredient.LittleIngredient;
 import team.creative.littletiles.common.ingredient.LittleIngredients;
 import team.creative.littletiles.common.ingredient.LittleInventory;
+import team.creative.littletiles.common.ingredient.NotEnoughIngredientsException.NotEnoughSpaceException;
 import team.creative.littletiles.common.item.ItemBlockIngredient;
 import team.creative.littletiles.common.item.ItemColorIngredient;
 import team.creative.littletiles.common.item.ItemColorIngredient.ColorIngredientType;
@@ -90,71 +97,6 @@ public class GuiBag extends GuiConfigure {
                     reloadControls();
                 } else if (event.source.name.startsWith("input")) {
                     
-                    ItemStack input = ((SlotControl) event.source).slot.getStack();
-                    
-                    if (input.getItem() instanceof ILittleIngredientInventory) {
-                        
-                        LittleIngredients ingredients = ((ILittleIngredientInventory) input.getItem()).getInventory(input);
-                        
-                        boolean containsBlocks = ingredients.contains(BlockIngredient.class);
-                        boolean containsColor = ingredients.contains(ColorIngredient.class);
-                        
-                        LittleIngredients remaining = bag.add(ingredients);
-                        ((ItemLittleBag) stack.getItem()).setInventory(stack, bag, null);
-                        
-                        if (remaining == null)
-                            remaining = new LittleIngredients();
-                        
-                        if (remaining.copy().sub(ingredients.copy()) != null) {
-                            if (containsBlocks) {
-                                updateSlots();
-                                player.playSound(SoundEvents.ITEM_FRAME_PLACE, 1.0F, 1.0F);
-                            }
-                            
-                            if (containsColor) {
-                                reloadControls();
-                                player.playSound(SoundEvents.BREWING_STAND_BREW, 1.0F, 1.0F);
-                            }
-                        }
-                        
-                        ((ILittleIngredientInventory) input.getItem()).setInventory(input, remaining, null);
-                    } else {
-                        LittleIngredients ingredients = LittleIngredient.extractWithoutCount(input, true);
-                        if (ingredients != null) {
-                            ingredients.scale(input.getCount());
-                            
-                            boolean containsBlocks = ingredients.contains(BlockIngredient.class);
-                            boolean containsColor = ingredients.contains(ColorIngredient.class);
-                            
-                            LittleIngredients overflow = bag.add(ingredients);
-                            if (overflow == null || (!overflow.contains(BlockIngredient.class) && !overflow.contains(ColorIngredient.class))) {
-                                
-                                input.setCount(0);
-                                ((ItemLittleBag) stack.getItem()).setInventory(stack, bag, null);
-                                
-                                LittleInventory inventory = new LittleInventory(player);
-                                try {
-                                    if (overflow != null)
-                                        inventory.give(overflow);
-                                } catch (NotEnoughSpaceException e) {}
-                                
-                                if (containsBlocks) {
-                                    updateSlots();
-                                    player.playSound(SoundEvents.ITEM_FRAME_PLACE, 1.0F, 1.0F);
-                                }
-                                
-                                if (containsColor) {
-                                    reloadControls();
-                                    player.playSound(SoundEvents.BREWING_STAND_BREW, 1.0F, 1.0F);
-                                }
-                                
-                            } else
-                                bag = ((ItemLittleBag) stack.getItem()).getInventory(stack);
-                            
-                        }
-                    }
-                    if (player instanceof EntityPlayerMP)
-                        ((EntityPlayerMP) player).sendContainerToPlayer(player.inventoryContainer);
                 }
             }
         });*/
@@ -177,76 +119,125 @@ public class GuiBag extends GuiConfigure {
         upper.add(left);
         
         GuiParent right = new GuiParent(GuiFlow.STACK_Y);
-        upper.add(right);
+        upper.add(right.setAlign(Align.STRETCH).setExpandableX());
         
+        right.add(new GuiInventoryGrid("input", input).addListener(x -> {
+            Player player = getPlayer();
+            ItemStack input = GuiBag.this.input.getItem(0);
+            
+            if (input.getItem() instanceof ILittleIngredientInventory) {
+                
+                LittleIngredients ingredients = ((ILittleIngredientInventory) input.getItem()).getInventory(input);
+                
+                boolean containsBlocks = ingredients.contains(BlockIngredient.class);
+                boolean containsColor = ingredients.contains(ColorIngredient.class);
+                
+                LittleIngredients remaining = bag.add(ingredients);
+                ((ItemLittleBag) tool.get().getItem()).setInventory(tool.get(), bag, null);
+                
+                if (remaining == null)
+                    remaining = new LittleIngredients();
+                
+                if (remaining.copy().sub(ingredients.copy()) != null) {
+                    if (containsBlocks) {
+                        updateSlots();
+                        player.playSound(SoundEvents.ITEM_FRAME_PLACE, 1.0F, 1.0F);
+                    }
+                    
+                    if (containsColor) {
+                        player.playSound(SoundEvents.BREWING_STAND_BREW, 1.0F, 1.0F);
+                        RELOAD.send(EndTag.INSTANCE);
+                    }
+                }
+                
+                ((ILittleIngredientInventory) input.getItem()).setInventory(input, remaining, null);
+            } else {
+                LittleIngredients ingredients = LittleIngredient.extractWithoutCount(input, true);
+                if (ingredients != null) {
+                    ingredients.scale(input.getCount());
+                    
+                    boolean containsBlocks = ingredients.contains(BlockIngredient.class);
+                    boolean containsColor = ingredients.contains(ColorIngredient.class);
+                    
+                    LittleIngredients overflow = bag.add(ingredients);
+                    if (overflow == null || (!overflow.contains(BlockIngredient.class) && !overflow.contains(ColorIngredient.class))) {
+                        
+                        input.setCount(0);
+                        ((ItemLittleBag) tool.get().getItem()).setInventory(tool.get(), bag, null);
+                        
+                        LittleInventory inventory = new LittleInventory(player);
+                        try {
+                            if (overflow != null)
+                                inventory.give(overflow);
+                        } catch (NotEnoughSpaceException e) {}
+                        
+                        if (containsBlocks) {
+                            updateSlots();
+                            player.playSound(SoundEvents.ITEM_FRAME_PLACE, 1.0F, 1.0F);
+                        }
+                        
+                        if (containsColor) {
+                            player.playSound(SoundEvents.BREWING_STAND_BREW, 1.0F, 1.0F);
+                            RELOAD.send(EndTag.INSTANCE);
+                        }
+                        
+                    } else
+                        bag = ((ItemLittleBag) tool.get().getItem()).getInventory(tool.get());
+                    
+                }
+            }
+            
+        }));
         right.add(new GuiColorProgressBar("black", unit.black, ItemLittleBag.colorUnitMaximum, Color.BLACK));
         right.add(new GuiColorProgressBar("cyan", unit.cyan, ItemLittleBag.colorUnitMaximum, Color.CYAN));
         right.add(new GuiColorProgressBar("magenta", unit.magenta, ItemLittleBag.colorUnitMaximum, Color.MAGENTA));
         right.add(new GuiColorProgressBar("yellow", unit.yellow, ItemLittleBag.colorUnitMaximum, Color.YELLOW));
         
         bag = ((ItemLittleBag) tool.get().getItem()).getInventory(tool.get());
-        /*List<BlockIngredientEntry> inventory = bag.get(BlockIngredient.class).getContent();
+        List<BlockIngredientEntry> inventory = bag.get(BlockIngredient.class).getContent();
         
         bagInventory = new SimpleContainer(ItemLittleBag.inventorySize) {
             @Override
-            public int getInventoryStackLimit() {
+            public int getMaxStackSize() {
                 return ItemLittleBag.maxStackSizeOfTiles;
             }
         };
-        for (int y = 0; y < ItemLittleBag.inventoryHeight; y++) {
-            for (int x = 0; x < ItemLittleBag.inventoryWidth; x++) {
-                int index = x + y * ItemLittleBag.inventoryWidth;
-                
-                ItemStack stack;
-                if (index < inventory.size()) {
-                    stack = new ItemStack(LittleTiles.blockIngredient);
-                    stack.setTagCompound(new NBTTagCompound());
-                    ItemBlockIngredient.saveIngredient(stack, inventory.get(index));
-                } else
-                    stack = ItemStack.EMPTY;
-                
-                bagInventory.setInventorySlotContents(index, stack);
-                controls.add(new SlotControlBlockIngredient(new Slot(bagInventory, index, 5 + x * 18, 5 + y * 18) {
-                    @Override
-                    public boolean isItemValid(ItemStack stack) {
-                        return false;
-                    }
-                }));
+        left.add(new GuiInventoryGrid(name, bagInventory, ItemLittleBag.inventoryWidth, ItemLittleBag.inventoryHeight, (c, i) -> new Slot(c, i, 0, 0) {
+            
+            @Override
+            public boolean mayPlace(ItemStack stack) {
+                return false;
             }
-        }
-        addSlotToContainer(new Slot(input, 0, 120, 5));*/
+            
+        }).addListener(x -> {
+            updateSlots();
+        }));
         
-        add(new GuiPlayerInventoryGrid(getPlayer()));
+        for (int i = 0; i < inventory.size(); i++) {
+            ItemStack stack = new ItemStack(LittleTilesRegistry.BLOCK_INGREDIENT.get());
+            stack.getOrCreateTag();
+            ItemBlockIngredient.saveIngredient(stack, inventory.get(i));
+            bagInventory.setItem(i, stack);
+        }
+        
+        add(new GuiPlayerInventoryGrid(getPlayer()).disableSlot(tool.index));
     }
     
     public void updateSlots() {
         List<BlockIngredientEntry> inventory = bag.get(BlockIngredient.class).getContent();
-        for (int y = 0; y < ItemLittleBag.inventoryHeight; y++) {
-            for (int x = 0; x < ItemLittleBag.inventoryWidth; x++) {
-                int index = x + y * ItemLittleBag.inventoryWidth;
-                
-                ItemStack stack;
-                if (index < inventory.size()) {
-                    stack = new ItemStack(LittleTiles.blockIngredient);
-                    stack.setTagCompound(new NBTTagCompound());
-                    ItemBlockIngredient.saveIngredient(stack, inventory.get(index));
-                } else
-                    stack = ItemStack.EMPTY;
-                
-                bagInventory.setInventorySlotContents(index, stack);
-            }
+        for (int i = 0; i < inventory.size(); i++) {
+            ItemStack stack = new ItemStack(LittleTilesRegistry.BLOCK_INGREDIENT.get());
+            stack.getOrCreateTag();
+            ItemBlockIngredient.saveIngredient(stack, inventory.get(i));
+            bagInventory.setItem(i, stack);
         }
-        if (player instanceof EntityPlayerMP)
-            ((EntityPlayerMP) player).sendContainerToPlayer(player.inventoryContainer);
     }
     
     @Override
     public CompoundTag saveConfiguration(CompoundTag nbt) {
-        //ItemStack stack = ((SlotControl) get("input0")).slot.getStack();
-        //if (!stack.isEmpty())
-        //    LevelUtils.dropItem(getPlayer(), stack);
-        //if (getPlayer() instanceof ServerPlayer sPlayer)
-        //    sPlayer.sendContainerToPlayer(player.inventoryContainer);
+        ItemStack stack = input.getItem(0);
+        if (!stack.isEmpty())
+            getPlayer().drop(stack, true);
         return null;
     }
     
