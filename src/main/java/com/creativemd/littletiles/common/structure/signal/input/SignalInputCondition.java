b4bd1f4f01959f589278717d5c87412da93ae8cc
_Doc <ldoc256@gmail.com>
2022-09-22 11:03:01 -0400

    Temp fix to virtual input of 1 not outputting all 1s

--- src/main/java/com/creativemd/littletiles/common/structure/signal/input/SignalInputCondition.java
+++ src/main/java/com/creativemd/littletiles/common/structure/signal/input/SignalInputCondition.java
@@ -223,7 +223,7 @@ public abstract class SignalInputCondition {
         
         @Override
         public boolean[] test(LittleStructure structure, boolean forceBitwise) {
-            return BooleanUtils.toBits(number, Math.max(1, BooleanUtils.getRequiredBandwidth(number)));
+            return BooleanUtils.toBits(number, 32);
         }
         
         @Override
