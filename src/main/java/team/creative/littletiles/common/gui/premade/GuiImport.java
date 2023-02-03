67224b9465f440719262befebc68a6fb79c609e2
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-02-03 12:31:38 +0100

    Fixed GuiImport

--- src/main/java/team/creative/littletiles/common/gui/premade/GuiImport.java
+++ src/main/java/team/creative/littletiles/common/gui/premade/GuiImport.java
@@ -1,12 +1,8 @@
 package team.creative.littletiles.common.gui.premade;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.TagParser;
 import net.minecraft.network.chat.Component;
@@ -24,6 +20,7 @@ import team.creative.creativecore.common.gui.dialog.DialogGuiLayer.DialogButton;
 import team.creative.creativecore.common.gui.dialog.GuiDialogHandler;
 import team.creative.creativecore.common.gui.flow.GuiFlow;
 import team.creative.creativecore.common.gui.sync.GuiSyncLocal;
+import team.creative.littletiles.LittleTilesRegistry;
 import team.creative.littletiles.common.grid.LittleGrid;
 import team.creative.littletiles.common.item.ItemLittleBlueprint;
 
@@ -35,34 +32,38 @@ public class GuiImport extends GuiLayer {
     public GuiSyncLocal<CompoundTag> IMPORT_DATA = getSyncHolder().register("import_data", (nbt) -> {
         ItemStack stack = importSlot.getItem(0);
         if (stack.getItem() instanceof ItemLittleBlueprint || (getPlayer().isCreative() && stack.isEmpty())) {
-            // TODO Import
-            //importSlot.setItem(0, newStack);
+            if (stack.isEmpty())
+                importSlot.setItem(0, stack = new ItemStack(LittleTilesRegistry.BLUEPRINT.get()));
+            
+            try {
+                LittleGrid.get(nbt);
+                CompoundTag stackTag = stack.getOrCreateTag();
+                stackTag.put(ItemLittleBlueprint.CONTENT_KEY, nbt);
+                get("import", GuiInventoryGrid.class).setChanged();
+            } catch (RuntimeException e) {}
         }
     });
     
     public GuiImport() {
         super("little.import");
         flow = GuiFlow.STACK_Y;
+        align = Align.STRETCH;
     }
     
     @Override
     public void create() {
-        add(textfield = new GuiTextfield("import"));
+        add(textfield = new GuiTextfield("import_textfield"));
+        textfield.setMaxStringLength(Integer.MAX_VALUE);
         GuiParent secondRow = new GuiParent();
-        secondRow.align = Align.RIGHT;
-        secondRow.add(new GuiButton("Paste", x -> {
-            Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
-            Transferable t = clpbrd.getContents(this);
-            if (t == null)
-                return;
-            try {
-                textfield.setText((String) t.getTransferData(DataFlavor.stringFlavor));
-            } catch (Exception e) {}
-            
-        }));
+        add(secondRow);
+        secondRow.add(new GuiButton("paste_button", x -> {
+            String text = Minecraft.getInstance().keyboardHandler.getClipboard();
+            if (text == null)
+                text = "";
+            textfield.setText(text);
+        }).setTranslate("gui.paste"));
         
-        secondRow.add(new GuiButton("Import", x -> {
-            
+        secondRow.add(new GuiButton("import_button", x -> {
             try {
                 CompoundTag nbt = TagParser.parseTag(textfield.getText());
                 try {
@@ -72,11 +73,13 @@ public class GuiImport extends GuiLayer {
                     GuiDialogHandler
                             .openDialog(getIntegratedParent(), "invalid_grid", Component.translatable("invalid_grid", nbt.getString("grid")), (y, z) -> {}, DialogButton.OK);
                 }
-            } catch (CommandSyntaxException e) {}
-        }));
+            } catch (CommandSyntaxException e) {
+                e.printStackTrace();
+            }
+        }).setTranslate("gui.import"));
         
         add(new GuiInventoryGrid("import", importSlot));
         
-        add(new GuiPlayerInventoryGrid(getPlayer()));
+        add(new GuiPlayerInventoryGrid(getPlayer()).setUnexpandableX());
     }
 }
