c332ca3c4a05a3207de7cc6006215accac4ed12b
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-02-06 15:51:42 +0100

    Fixed LittleStructure.isClient() not working in all cases

--- src/main/java/team/creative/littletiles/common/structure/LittleStructure.java
+++ src/main/java/team/creative/littletiles/common/structure/LittleStructure.java
@@ -118,7 +118,7 @@ public abstract class LittleStructure implements ISignalSchedulable, ILevelPosit
     }
     
     public boolean isClient() {
-        return getLevel().isClientSide;
+        return mainBlock != null && mainBlock.isClient();
     }
     
     @Override
