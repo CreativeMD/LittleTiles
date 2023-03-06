2e5366d1ae7a2624cdc781af6210722cc2d8be65
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-03-06 16:57:11 +0100

    Fixed sending blocks twice in animation init packet

--- src/main/java/team/creative/littletiles/common/packet/entity/animation/LittleAnimationInitPacket.java
+++ src/main/java/team/creative/littletiles/common/packet/entity/animation/LittleAnimationInitPacket.java
@@ -1,11 +1,7 @@
 package team.creative.littletiles.common.packet.entity.animation;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Player;
-import team.creative.littletiles.common.block.entity.BETiles;
 import team.creative.littletiles.common.entity.animation.LittleAnimationEntity;
 import team.creative.littletiles.common.packet.entity.LittleEntityPacket;
 import team.creative.littletiles.common.structure.relative.StructureAbsolute;
@@ -13,7 +9,6 @@ import team.creative.littletiles.common.structure.relative.StructureAbsolute;
 public class LittleAnimationInitPacket extends LittleEntityPacket<LittleAnimationEntity> {
     
     public StructureAbsolute absolute;
-    public List<LittleBlockChange> blocks;
     public CompoundTag extraData;
     
     public LittleAnimationInitPacket() {}
@@ -22,9 +17,6 @@ public class LittleAnimationInitPacket extends LittleEntityPacket<LittleAnimatio
         super(entity);
         this.absolute = entity.getCenter();
         this.extraData = entity.saveExtraClientData();
-        this.blocks = new ArrayList<>();
-        for (BETiles block : entity.getSubLevel())
-            blocks.add(new LittleBlockChange(block));
     }
     
     @Override
