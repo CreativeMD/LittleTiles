fcc7213376e7b05c689a4754d1d024cb88036d14
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-03-23 13:50:14 +0100

    Added ability to open doors with hand

--- src/main/java/team/creative/littletiles/common/structure/type/animation/LittleUndirectedStateStructure.java
+++ src/main/java/team/creative/littletiles/common/structure/type/animation/LittleUndirectedStateStructure.java
@@ -3,7 +3,13 @@ package team.creative.littletiles.common.structure.type.animation;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import team.creative.littletiles.common.block.little.tile.LittleTileContext;
 import team.creative.littletiles.common.block.little.tile.parent.IStructureParentCollection;
 import team.creative.littletiles.common.structure.LittleStructureType;
 import team.creative.littletiles.common.structure.animation.AnimationState;
@@ -60,6 +66,20 @@ public abstract class LittleUndirectedStateStructure extends LittleStateStructur
     public void performInternalOutputChange(InternalSignalOutput output) {
         if (output.component.is("state"))
             startTransitionIfNecessary(output);
+    }
+    
+    @Override
+    public boolean canInteract() {
+        return canRightClick();
+    }
+    
+    @Override
+    public InteractionResult use(Level level, LittleTileContext context, BlockPos pos, Player player, BlockHitResult result) {
+        if (canRightClick()) {
+            getOutput(0).toggle();
+            return InteractionResult.SUCCESS;
+        }
+        return super.use(level, context, pos, player, result);
     }
     
 }
