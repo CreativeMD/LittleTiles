43a94292bee81af787734e73e25a6f407e5fe8ad
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-07-05 23:32:47 +0200

    Fixed incorrect colors when using rubidium

--- src/main/java/team/creative/littletiles/mixin/rubidium/BlockRendererMixin.java
+++ src/main/java/team/creative/littletiles/mixin/rubidium/BlockRendererMixin.java
@@ -16,6 +16,7 @@ import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRende
 import me.jellysquid.mods.sodium.client.render.vertex.type.ChunkVertexBufferBuilder;
 import me.jellysquid.mods.sodium.client.render.vertex.type.ChunkVertexEncoder;
 import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
+import me.jellysquid.mods.sodium.client.util.color.ColorARGB;
 import net.minecraft.world.phys.Vec3;
 
 @Mixin(BlockRenderer.class)
@@ -27,10 +28,10 @@ public class BlockRendererMixin {
     private void mulColor(BlockRenderContext ctx, ChunkVertexBufferBuilder vertexBuffer, IndexBufferBuilder indexBuffer, Vec3 offset, ModelQuadView quad, int[] colors, float[] brightness, int[] lightmap, CallbackInfo info, ModelQuadOrientation orientation, ChunkVertexEncoder.Vertex[] vertices, int dstIndex, int srcIndex, ChunkVertexEncoder.Vertex out) {
         int color = colors != null ? colors[srcIndex] : quad.getColor(srcIndex);
         float w = brightness[srcIndex];
-        float r = ColorABGR.unpackRed(color) * w;
-        float g = ColorABGR.unpackGreen(color) * w;
-        float b = ColorABGR.unpackBlue(color) * w;
-        int a = ColorABGR.unpackAlpha(color);
+        float r = ColorARGB.unpackRed(color) * w;
+        float g = ColorARGB.unpackGreen(color) * w;
+        float b = ColorARGB.unpackBlue(color) * w;
+        int a = ColorARGB.unpackAlpha(color);
         out.color = ColorABGR.pack((int) r, (int) g, (int) b, a);
     }
     
