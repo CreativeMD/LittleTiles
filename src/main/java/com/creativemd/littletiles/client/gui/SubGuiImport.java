70a8f3edffc2c5cf8b33567211c9ace35630bc0f
_Doc <ldoc256@gmail.com>
2023-08-01 13:12:38 -0400

    Option to prevent survival mode players from importing structure

--- src/main/java/com/creativemd/littletiles/client/gui/SubGuiImport.java
+++ src/main/java/com/creativemd/littletiles/client/gui/SubGuiImport.java
@@ -9,6 +9,9 @@ import java.awt.datatransfer.Transferable;
 import com.creativemd.creativecore.common.gui.container.SubGui;
 import com.creativemd.creativecore.common.gui.controls.gui.GuiButton;
 import com.creativemd.creativecore.common.gui.controls.gui.GuiTextfield;
+import com.creativemd.littletiles.common.structure.registry.LittleStructureType;
+import com.creativemd.littletiles.common.tile.preview.LittlePreviews;
+import com.creativemd.littletiles.common.util.converation.StructureStringUtils;
 import com.creativemd.littletiles.common.util.grid.LittleGridContext;
 
 import net.minecraft.nbt.JsonToNBT;
@@ -35,11 +38,11 @@ public class SubGuiImport extends SubGui {
                 try {
                     textfield.text = (String) t.getTransferData(DataFlavor.stringFlavor);
                 } catch (Exception e) {
-                
+                    
                 }
             }
         });
-                
+        
         controls.add(new GuiButton("Import", 128, 52) {
             
             @Override
@@ -48,15 +51,39 @@ public class SubGuiImport extends SubGui {
                     NBTTagCompound nbt = JsonToNBT.getTagFromJson(textfield.text);
                     try {
                         LittleGridContext.get(nbt);
-                        sendPacketToServer(nbt);
+                        String name = canImport(LittlePreviews.getPreview(StructureStringUtils.importStructure(nbt), false));
+                        if (name.isEmpty())
+                            sendPacketToServer(nbt);
+                        else
+                            openButtonDialogDialog("Structure Type: " + name + " must be imported in creative mode.", "Ok");
                     } catch (RuntimeException e) {
                         openButtonDialogDialog("Invalid grid size " + nbt.getInteger("grid"), "Ok");
                     }
                 } catch (NBTException e) {
-                
+                    
                 }
             }
         });
     }
     
+    /** Gets the name of the structure type that is preventing the player from importing the structure in survival mode
+     * 
+     * @param previews
+     *            Previews that is looks through
+     * @return
+     *         The name of the structure that is preventing the player from importing */
+    private String canImport(LittlePreviews previews) {
+        LittleStructureType type = previews.getStructureType();
+        if (!type.canImport())
+            return type.id;
+        String typeName = "";
+        for (LittlePreviews child : previews.getChildren()) {
+            LittleStructureType typeChild = child.getStructureType();
+            if (!typeChild.canImport()) {
+                typeName = typeChild.id;
+                break;
+            }
+        }
+        return typeName;
+    }
 }
