7ff67a2c8a9904111e8c35e1e2a29113785f2c42
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-02-10 13:43:44 +0100

    Fixed placement check if there is enough space having troubles with different grids

--- src/main/java/team/creative/littletiles/common/placement/Placement.java
+++ src/main/java/team/creative/littletiles/common/placement/Placement.java
@@ -419,7 +419,7 @@ public class Placement {
                 if (size > LittleTiles.CONFIG.general.maxAllowedDensity)
                     throw new LittleTilesConfig.TooDenseException();
                 
-                if (!te.sameGrid(te, () -> {
+                if (!te.sameGrid(this, () -> {
                     for (int i = 0; i < tiles.length; i++)
                         if (tiles[i] != null)
                             for (LittleTile tile : tiles[i])
