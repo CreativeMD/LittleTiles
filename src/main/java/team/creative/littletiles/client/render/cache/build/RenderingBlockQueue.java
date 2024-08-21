975503404f18391914496d6111b0ed2ae4f7157d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-08-21 08:43:19 +0200

    Fixed missing certain files on github

---
+++ src/main/java/team/creative/littletiles/client/render/cache/build/RenderingBlockQueue.java
@@ -0,0 +1,79 @@
+package team.creative.littletiles.client.render.cache.build;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import it.unimi.dsi.fastutil.longs.Long2IntMap;
+import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.minecraft.world.level.Level;
+import team.creative.littletiles.client.render.mc.RenderChunkExtender;
+import team.creative.littletiles.common.block.entity.BETiles;
+
+public class RenderingBlockQueue {
+    
+    private ConcurrentLinkedQueue<RenderingBlockContext> queue = new ConcurrentLinkedQueue<>();
+    private Object2ObjectMap<Level, Long2IntMap> levels = new Object2ObjectOpenHashMap<>();
+    
+    public void requeue(RenderingBlockContext context) {
+        queue.add(context);
+    }
+    
+    public synchronized void queue(BETiles tiles, long pos) {
+        var level = tiles.getLevel();
+        var handler = RenderingLevelHandler.of(level);
+        pos = handler.prepareQueue(pos);
+        RenderingBlockContext context = new RenderingBlockContext(tiles, pos, handler);
+        var sections = levels.computeIfAbsent(level, x -> new Long2IntOpenHashMap());
+        int count = sections.getOrDefault(pos, 0);
+        count++;
+        sections.put(pos, count);
+        queue.add(context);
+    }
+    
+    public synchronized RenderChunkExtender unqeue(RenderingBlockContext context) {
+        var level = context.getLevel();
+        var sections = levels.get(level);
+        if (sections == null)
+            return null;
+        int count = sections.get(context.pos);
+        count--;
+        if (count <= 0) {
+            sections.remove(context.pos);
+            if (sections.isEmpty())
+                levels.remove(level);
+            return context.getRenderChunk();
+        }
+        sections.put(context.pos, count);
+        return null;
+    }
+    
+    public RenderingBlockContext poll() {
+        return queue.poll();
+    }
+    
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+    
+    public int levelCount() {
+        return levels.size();
+    }
+    
+    public int sectionCount() {
+        int sections = 0;
+        for (Long2IntMap map : levels.values())
+            sections += map.size();
+        return sections;
+    }
+    
+    public int size() {
+        return queue.size();
+    }
+    
+    public void clear() {
+        levels.clear();
+        queue.clear();
+    }
+    
+}
---
+++ src/main/java/team/creative/littletiles/client/render/cache/build/RenderingLevelHandler.java
@@ -0,0 +1,137 @@
+package team.creative.littletiles.client.render.cache.build;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.BlockPos.MutableBlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import team.creative.creativecore.common.level.ISubLevel;
+import team.creative.littletiles.client.level.little.FakeClientLevel;
+import team.creative.littletiles.client.mod.embeddium.EmbeddiumManager;
+import team.creative.littletiles.client.render.cache.pipeline.LittleRenderPipelineType;
+import team.creative.littletiles.client.render.mc.RenderChunkExtender;
+import team.creative.littletiles.client.render.mc.ViewAreaExtender;
+import team.creative.littletiles.common.level.little.LittleLevel;
+import team.creative.littletiles.mixin.client.render.LevelRendererAccessor;
+
+public abstract class RenderingLevelHandler {
+    
+    public static Vec3 offsetCorrection(Vec3i to, Vec3i from) {
+        if (to == from || to.equals(from))
+            return null;
+        return new Vec3(from.getX() - to.getX(), from.getY() - to.getY(), from.getZ() - to.getZ());
+    }
+    
+    public static Vec3 offsetCorrection(Vec3i to, int x, int y, int z) {
+        if (to.getX() == x && to.getY() == y && to.getZ() == z)
+            return null;
+        return new Vec3(x - to.getX(), y - to.getY(), z - to.getZ());
+    }
+    
+    public static Vec3 offsetCorrection(RenderingLevelHandler target, RenderingLevelHandler origin, SectionPos pos) {
+        var targetOffset = target.standardOffset(pos);
+        var originOffset = origin.standardOffset(pos);
+        if (targetOffset != null && originOffset != null)
+            return offsetCorrection(targetOffset, originOffset);
+        return null;
+    }
+    
+    public static final RenderingLevelHandler VANILLA = new RenderingLevelHandler() {
+        
+        @Override
+        public LittleRenderPipelineType getPipeline() {
+            return LittleRenderPipelineType.FORGE;
+        }
+        
+        @Override
+        public RenderChunkExtender getRenderChunk(Level level, long pos) {
+            return (RenderChunkExtender) ((ViewAreaExtender) ((LevelRendererAccessor) Minecraft.getInstance().levelRenderer).getViewArea()).getSection(pos);
+        }
+        
+        @Override
+        public BlockPos standardOffset(SectionPos pos) {
+            return pos.origin();
+        }
+    };
+    
+    public static final RenderingLevelHandler ENTITY = new RenderingLevelHandler() {
+        
+        @Override
+        public LittleRenderPipelineType getPipeline() {
+            return LittleRenderPipelineType.FORGE;
+        }
+        
+        @Override
+        public RenderChunkExtender getRenderChunk(Level level, long pos) {
+            return ((LittleLevel) level).getRenderManager().getRenderChunk(pos);
+        }
+        
+        @Override
+        public BlockPos standardOffset(SectionPos pos) {
+            return pos.origin();
+        }
+    };
+    
+    public static final RenderingLevelHandler ANIMATION = new RenderingLevelHandler() {
+        
+        @Override
+        public LittleRenderPipelineType getPipeline() {
+            return LittleRenderPipelineType.FORGE;
+        }
+        
+        @Override
+        public void prepareModelOffset(Level level, MutableBlockPos modelOffset, BlockPos pos) {
+            modelOffset.set(pos.getX(), pos.getY(), pos.getZ());
+        }
+        
+        @Override
+        public RenderChunkExtender getRenderChunk(Level level, long pos) {
+            return ((LittleLevel) level).getRenderManager().getRenderChunk(pos);
+        }
+        
+        @Override
+        public BlockPos standardOffset(SectionPos pos) {
+            return BlockPos.ZERO;
+        }
+        
+        @Override
+        public long prepareQueue(long pos) {
+            return 0;
+        }
+    };
+    
+    public static RenderingLevelHandler of(Level level) {
+        if (EmbeddiumManager.installed())
+            if (level instanceof LittleLevel l && l.getRenderManager().isSmall())
+                if (l instanceof ISubLevel s && s.getParent() instanceof FakeClientLevel)
+                    return ANIMATION;
+                else
+                    return EmbeddiumManager.RENDERING_ANIMATION;
+            else
+                return EmbeddiumManager.RENDERING_LEVEL;
+        if (level instanceof LittleLevel l)
+            return l.getRenderManager().isSmall() ? ANIMATION : ENTITY;
+        return VANILLA;
+    }
+    
+    public abstract LittleRenderPipelineType getPipeline();
+    
+    public void prepareModelOffset(Level level, MutableBlockPos modelOffset, BlockPos pos) {
+        modelOffset.set(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15);
+    }
+    
+    public long prepareQueue(long pos) {
+        return pos;
+    }
+    
+    public abstract RenderChunkExtender getRenderChunk(Level level, long pos);
+    
+    public int sectionIndex(Level level, long pos) {
+        return -1;
+    }
+    
+    public abstract BlockPos standardOffset(SectionPos pos);
+    
+}
