347d491c915f08da320f48627a6e875fe585d0c7
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-09-12 13:51:24 +0200

    Fixed being able to insert certain blocks (like shulker box) into little bag

--- src/main/java/team/creative/littletiles/common/action/LittleAction.java
+++ src/main/java/team/creative/littletiles/common/action/LittleAction.java
@@ -447,23 +447,24 @@ public abstract class LittleAction<T> extends CreativePacket {
     }
     
     private static boolean isBlockValid(Block block) {
-        if (block instanceof EntityBlock || block instanceof SlabBlock)
-            return false;
+        
         if (LittleBlockRegistry.isSpecialBlock(block))
             return true;
         return block instanceof TransparentBlock || block instanceof StainedGlassBlock || block instanceof HalfTransparentBlock || block instanceof LeavesBlock;
     }
     
     private static boolean isBlockInvalid(Block block) {
+        if (block instanceof EntityBlock || block instanceof SlabBlock)
+            return true;
         var location = block.builtInRegistryHolder().unwrapKey().get().location();
         return location.getNamespace().equals("framedblocks");
     }
     
     public static boolean isBlockValid(BlockState state) {
-        if (isBlockValid(state.getBlock()))
-            return true;
         if (isBlockInvalid(state.getBlock()))
             return false;
+        if (isBlockValid(state.getBlock()))
+            return true;
         
         if (state.isSolid() && state.isCollisionShapeFullBlock(EmptyBlockGetter.INSTANCE, BlockPos.ZERO))
             return true;
