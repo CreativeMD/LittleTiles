43d478be74d8677f5fe9b722c3f40c5867b676f4
Deepseasaltyfish <h20040215@163.com>
2025-12-01 17:31:18 +0800

    backport 1.21.1 commit 66f7374

--- src/main/java/team/creative/littletiles/common/structure/LittleStructure.java
+++ src/main/java/team/creative/littletiles/common/structure/LittleStructure.java
@@ -823,6 +823,8 @@ public abstract class LittleStructure implements ISignalSchedulable, ILevelPosit
     }
     
     public SignalExternalOutputHandler getExternalOutput(int index) {
+        if (externalHandler == null)
+            return null;
         return externalHandler.get(index);
     }
     
