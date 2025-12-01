96e05c3d37fe2dd6d848478ca7e769a804c3c52e
Deepseasaltyfish <h20040215@163.com>
2025-12-01 17:22:23 +0800

    backport 1.21.1 commit e0a7573

--- src/main/java/team/creative/littletiles/common/structure/type/premade/signal/LittleSignalCableBase.java
+++ src/main/java/team/creative/littletiles/common/structure/type/premade/signal/LittleSignalCableBase.java
@@ -495,7 +495,8 @@ public abstract class LittleSignalCableBase extends LittleStructurePremade imple
                     if (faces[i] != null) {
                         ISignalStructureBase connection = faces[i].connection;
                         faces[i].disconnect(getFacing(i));
-                        connection.findNetwork();
+                        if (connection != null)
+                            connection.findNetwork();
                     }
                 }
             }
