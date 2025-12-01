a0855d4ac4e8041bfa33b837e57e2bc16bd9726e
Deepseasaltyfish <h20040215@163.com>
2025-12-01 17:50:56 +0800

    backport 1.21.1 commit 4519ccb

--- src/main/java/team/creative/littletiles/client/render/cache/build/RenderingThread.java
+++ src/main/java/team/creative/littletiles/client/render/cache/build/RenderingThread.java
@@ -58,6 +58,7 @@ public class RenderingThread extends Thread {
                 
             while (QUEUE.size() > 0)
                 QUEUE.poll().be.render.resetRenderingState();
+            QUEUE.clear(); // Make sure all levels and sections are reset
         }
         THREADS = new ArrayList<>();
         for (int i = 0; i < count; i++)
