cb4b48a2e354d0068474a70298e7ba213f017b05
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-05-29 11:09:21 +0200

    Fixed tile selection mode causing ghost tiles

--- src/main/java/team/creative/littletiles/common/placement/selection/TileSelectionMode.java
+++ src/main/java/team/creative/littletiles/common/placement/selection/TileSelectionMode.java
@@ -72,7 +72,7 @@ public class TileSelectionMode extends SelectionMode {
                     boxes.addBoxes(pair.key, pair.value);
             }
         } else
-            boxes.addBox(context.parent.getGrid(), hit.getBlockPos(), context.box);
+            boxes.addBox(context.parent.getGrid(), hit.getBlockPos(), context.box.copy());
         nbt.put("boxes", boxes.save(new CompoundTag()));
         return config.withConfig(nbt);
     }
@@ -93,7 +93,7 @@ public class TileSelectionMode extends SelectionMode {
         else if (secondMode)
             boxes.cutOut(LittleGrid.MIN, hit.getBlockPos(), LittleGrid.MIN.box());
         else
-            boxes.cutOut(context.parent.getGrid(), hit.getBlockPos(), context.box);
+            boxes.cutOut(context.parent.getGrid(), hit.getBlockPos(), context.box.copy());
         nbt.put("boxes", boxes.save(new CompoundTag()));
         return config.withConfig(nbt);
     }
